ad3a1c174657cb0ced9ea0f333b6821c3a58895a
halimkun <ffaisalhalim@gmail.com>
2022-10-27 19:39:57 +0700

    add refresh indicator

--- lib/pages/aduan.dart
+++ lib/pages/aduan.dart
@@ -33,8 +33,14 @@ class _AduanState extends State<Aduan> {
   @override
   void initState() {
     super.initState();
-    fetchAduan();
-    _aduan = fetchAduan();
+    refreshData();
+  }
+
+  Future<void> refreshData() async {
+    setState(() {
+      print('refreshing data');
+      _aduan = fetchAduan();
+    });
   }
 
   @override
@@ -45,146 +51,144 @@ class _AduanState extends State<Aduan> {
         if (snapshot.hasData) {
           List<Data>? data = snapshot.data!.data;
           if (data!.isEmpty) {
-            return Container(
-              alignment: Alignment.center,
-              child: const Card(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 26, horizontal: 29),
-                  child: Text(
-                    'Belum ada aduan',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.redAccent,
-                    ),
-                  ),
-                ),
-              ),
-            );
+            return const emptyData();
           } else {
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.grey[900],
-                      elevation: 1,
-                      padding: const EdgeInsets.all(8),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => Detail(
-                            nomor: data[index].nomor!,
-                          ),
-                        ),
-                      );
-                    },
-                    onLongPress: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text("Hapus Aduan : ${data[index].nomor!}"),
-                            content: Text(
-                                'Apakah anda yakin akan menghapus aduan berikut ini: \n\nTentang : ${data[index].judul!} \nNomor : ${data[index].nomor!.toString()} \nDiajukan : ${data[index].tanggal!.toString()}'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                style: TextButton.styleFrom(
-                                  foregroundColor: Colors.redAccent,
+            return RefreshIndicator(
+              onRefresh: () => Future.delayed(
+                const Duration(seconds: 1),
+                () => refreshData(),
+              ),
+              child: ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.grey[900],
+                        elevation: 1,
+                        padding: const EdgeInsets.all(8),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .push(
+                              MaterialPageRoute(
+                                builder: (context) => Detail(
+                                  nomor: data[index].nomor!,
                                 ),
-                                child: const Text('Ya'),
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('Batal'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            height: 100,
-                            color: Colors.grey[200],
-                            child: Image.network(
-                              '${Config().getBaseUrl}/foto_kejadian/${data[index].foto}',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data[index].judul!,
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
+                            )
+                            .then(
+                              (value) => refreshData(),
+                            );
+                      },
+                      onLongPress: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title:
+                                  Text("Hapus Aduan : ${data[index].nomor!}"),
+                              content: Text(
+                                  'Apakah anda yakin akan menghapus aduan berikut ini: \n\nTentang : ${data[index].judul!} \nNomor : ${data[index].nomor!.toString()} \nDiajukan : ${data[index].tanggal!.toString()}'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  style: TextButton.styleFrom(
+                                    foregroundColor: Colors.redAccent,
                                   ),
+                                  child: const Text('Ya'),
                                 ),
-                                const SizedBox(height: 8),
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Ionicons.time_outline,
-                                      size: 16,
-                                      color: Colors.grey[600],
-                                    ),
-                                    const SizedBox(width: 5),
-                                    Text(
-                                      data[index].tanggal!,
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 3),
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Ionicons.location_sharp,
-                                      size: 16,
-                                      color: Colors.grey[600],
-                                    ),
-                                    const SizedBox(width: 5),
-                                    Text(
-                                      data[index].lokasi!,
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Batal'),
                                 ),
                               ],
+                            );
+                          },
+                        );
+                      },
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 100,
+                              color: Colors.grey[200],
+                              child: Image.network(
+                                '${Config().getBaseUrl}/foto_kejadian/${data[index].foto}',
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 5),
-                          child: StatusGen(status: data[index].status!),
-                        ),
-                      ],
+                          Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data[index].judul!,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Ionicons.time_outline,
+                                        size: 16,
+                                        color: Colors.grey[600],
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(
+                                        data[index].tanggal!,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 3),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Ionicons.warning_outline,
+                                        size: 16,
+                                        color: Colors.grey[600],
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(
+                                        data[index].jenis!,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child:
+                                StatusGen().bullet(status: data[index].status!),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             );
           }
         } else if (snapshot.hasError) {
@@ -195,6 +199,31 @@ class _AduanState extends State<Aduan> {
           child: CircularProgressIndicator(),
         );
       },
+    );
+  }
+}
+
+class emptyData extends StatelessWidget {
+  const emptyData({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: const Card(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 26, horizontal: 29),
+          child: Text(
+            'Belum ada aduan',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.redAccent,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
